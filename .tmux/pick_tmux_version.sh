7fa4b62827fc2dcbc0535c3fdeb3e27f09f38bcd
Francesco Visin <fvisin@gmail.com>
2016-09-25 13:41:32 +0200

    Fix tmux on nvidia server

--- .tmux/pick_tmux_version.sh
+++ .tmux/pick_tmux_version.sh
@@ -2,10 +2,10 @@
 
 load_tmux_config () {
     tmux_home=~/.tmux
-    tmux_version="$(tmux -V | cut -c 6-)"
+    tmux_version="$(tmux -V | cut -d ' ' -f2)"
     
     if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
-        tmux source-file "$tmux_home/tmux_gt_2.1.conf"
+        tmux source-file "$tmux_home/tmux_ge_2.1.conf"
         exit
     elif [[ $(echo "$tmux_version >= 1.9" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/tmux_1.9_to_2.1.conf"
