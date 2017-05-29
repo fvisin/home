6115fbb49c98a273d24aa53bb4cea1c46850b270
Francesco Visin <fvisin@gmail.com>
2017-05-29 12:37:08 +0200

    Check that bc is installed or fallback to tmux<v1.9

--- .tmux/pick_tmux_version.sh
+++ .tmux/pick_tmux_version.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+ERROR_STR="'bc' is required to select the tmux version but it's not installed."
+ERROR_STR="$ERROR_STR Falling back to config lt1.9"
+
 load_tmux_config () {
     tmux_home=~/.tmux
     tmux_version="$(tmux -V | cut -d ' ' -f2)"
+
+    type bc >/dev/null 2>&1 || { echo >&2 $ERROR_STR;}
     
     if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/tmux_ge_2.1.conf"
